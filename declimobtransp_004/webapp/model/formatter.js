8a1395c81adb51b2352e52402e7c037b77e5eb9e
Wilian Ribeiro <wilian087@hotmail.com>
2022-12-08 14:52:02 -0300

    gap FI272

---
+++ declimobtransp_004/webapp/model/formatter.js
@@ -0,0 +1,47 @@
+sap.ui.define([] , function () {
+	"use strict";
+
+	return {
+        currencyInBRL: function(sValue) {
+            return Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(sValue) ;
+        },
+
+        realInAmount: function(sValue) {
+            sValue = sValue.replace("R$", "").replaceAll(".", "").trim();
+
+            sValue = sValue.replace(",", ".");
+
+            return sValue;
+        },
+
+        dateInUTCAbap: function(sValue, sPattern) {
+            return sap.ui.core.format.DateFormat.getDateTimeInstance({
+                pattern: sPattern,
+                UTC: false
+            }).format(sValue);
+        },
+
+        typeStatus: function(sValue) {
+            switch (sValue) {
+                case "E":
+                    return "sap-icon://message-error";
+                    break;
+                case "S":
+                    return "sap-icon://message-success";
+                    break;
+            }
+        },
+
+        typeStatusState: function(sValue) {
+            switch (sValue) {
+                case "E":
+                    return "Error";
+                    break;
+                case "S":
+                    return "Success";
+                    break;
+            }
+        }
+	};
+
+});
---
+++ declimobtransp_004/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comitsgroup.brz./declimobtransp/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
