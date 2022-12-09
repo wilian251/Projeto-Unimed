f32bfc1875c1275b2b8da2ca8d305cfd03b63cb1
Wilian Ribeiro <wilian087@hotmail.com>
2022-12-09 18:06:30 -0300

    ajuste tabela

--- declimobtransp_004/webapp/controller/Main.controller.js
+++ declimobtransp_004/webapp/controller/Main.controller.js
@@ -42,7 +42,7 @@ function (
             /* =========================================================== */
 
             onPressGenerete: function(oEvent) {
-                this.setAppBusy(true);
+                //this.setAppBusy(true);
 
                 let oItems = this.byId("SmartTable").getTable().getAggregation("items"),
                     bValid = false;
@@ -93,12 +93,138 @@ function (
                 });
             },
 
+            onPersoButtonPressed: function (oEvent) {
+                if(this._oTPC != null){
+                    this._oTPC.openDialog();
+                    this._oTPC.attachPersonalizationsDone(this, this._onPerscoDonePressed.bind(this));
+                }
+            },
+
             /* =========================================================== */
             /* internal methods                                            */
             /* =========================================================== */
             _onObjectMatched: async function(oEvent) { 
                 this.getModel("declimobtransp").setData(declimobtransp.initSelectionModel(this.getResourceBundle().getText("mainViewTableTitle")));
                 this.getModel("declimobtransp").refresh(true);
+            },
+
+            
+			_onPerscoDonePressed: function (oEvent) {
+                this._oTPC.savePersonalizations();
+            },
+
+            _createFilters: function() {
+                let oCompanyID          = this.byId("container-customertitles---main--smartFilterBar-filterItemControlA_-bukrs"),
+                    oClientID           = this.byId("container-customertitles---main--smartFilterBar-filterItemControlA_-kunnr"),
+                    oAccountingEntryID  = this.byId("container-customertitles---main--smartFilterBar-filterItemControlA_-blart"),
+                    oBusinessLocationID = this.byId("container-customertitles---main--smartFilterBar-filterItemControlA_-bupla");
+            
+                let oFilters = [];
+
+                if(oCompanyID === undefined){
+                    oCompanyID = this.byId("application-ZSEM_FI225_CUSTTITLES_001-display-component---main--smartFilterBar-filterItemControlA_-bukrs");
+                }
+
+                if(oClientID === undefined){
+                    oClientID = this.byId("application-ZSEM_FI225_CUSTTITLES_001-display-component---main--smartFilterBar-filterItemControlA_-kunnr");
+                }
+                
+                if(oAccountingEntryID === undefined){
+                    oAccountingEntryID  = this.byId("application-ZSEM_FI225_CUSTTITLES_001-display-component---main--smartFilterBar-filterItemControlA_-blart");
+                }
+                
+                if(oBusinessLocationID === undefined){
+                    oBusinessLocationID = this.byId("application-ZSEM_FI225_CUSTTITLES_001-display-component---main--smartFilterBar-filterItemControlA_-bupla");
+                }
+
+                if(oCompanyID.getTokens().length != 0){
+                    let oFilterCompany = this._searchSelectedValues(oCompanyID.getTokens(), "bukrs");
+
+                    oFilters.push(new Filter({
+                        filters: oFilterCompany,
+                        and: false
+                    }));
+                }
+
+                if(oClientID.getTokens().length != 0){
+                    let oFilterClient = this._searchSelectedValues(oClientID.getTokens(), "kunnr");
+
+                    oFilters.push(new Filter({
+                        filters: oFilterClient,
+                        and: false
+                    }));
+                }
+                
+                if(oAccountingEntryID.getTokens().length != 0){
+                    let oFilterAccountingEntry = this._searchSelectedValues(oAccountingEntryID.getTokens(), "blart");
+
+                    oFilters.push(new Filter({
+                        filters: oFilterAccountingEntry,
+                        and: false
+                    }));
+                }
+
+                if(oBusinessLocationID.getTokens().length != 0){
+                    let oFilterBusinessLocation = this._searchSelectedValues(oBusinessLocationID.getTokens(), "blart");
+
+                    oFilters.push(new Filter({
+                        filters: oFilterBusinessLocation,
+                        and: false
+                    }));
+                }
+
+                return oFilters;
+            },
+
+            _searchSelectedValues: function(sTokens, sPath){
+                let oFilter = [];
+
+                for(let oToken of sTokens){
+                    if(sPath === "kunnr") {
+                        oFilter.push(new Filter({
+                            path: sPath,
+                            operator: FilterOperator.EQ,
+                            value1: ("0000000000" + oToken.getProperty("key")).slice(-10),
+                            and: false
+                        }));
+                    }else{    
+                        oFilter.push(new Filter({
+                            path: sPath,
+                            operator: FilterOperator.EQ,
+                            value1: oToken.getProperty("key"),
+                            and: false
+                        }));
+
+                    }
+                }
+
+                return oFilter;
+            },
+
+            _personalizationTable: function () {
+                this._oTPC = null;
+                
+                let oPersonalizationService;
+
+                try {
+                    oPersonalizationService = sap.ushell.Container.getService("Personalization");
+                } catch (error) {
+                    
+                }
+
+                if(oPersonalizationService != undefined){
+                    let oPersonalizer = oPersonalizationService.getPersonalizer({
+                        container: "com.prestativ.unmd.declimobtransp", // This key must be globally unique (use a key to
+                        // identify the app) Note that only 40 characters are allowed
+                        item: "itemDataTable" // Maximum of 40 characters applies to this key as well
+                    });
+                    this._oTPC = new TablePersoController({
+                        table: this.byId("table"),
+                        //specify the first part of persistence ids e.g. 'demoApp-productsTable-dimensionsCol'
+                        componentName: "table",
+                        persoService: oPersonalizer
+                    }).activate();
+                }
             },
 
             _openDialogReturnBapis: async function(){
--- declimobtransp_004/webapp/model/declimobtransp.js
+++ declimobtransp_004/webapp/model/declimobtransp.js
@@ -6,47 +6,6 @@ sap.ui.define([
 	return {
 		initSelectionModel: function(sHeaderTitleTable) {
 			return {
-				id: "",
-				simpleForm: {
-					account: "",
-					corporateBank: "",
-					documentDate: null,
-					releaseDate: null,
-					accountingEntry: "",
-					headerText: "",
-					State: {
-						account: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						},
-						corporateBank: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						},
-						documentDate: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						},
-						releaseDate: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						},
-						accountingEntry: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						},
-						headerText: {
-							Enabled: true,
-							ValueState: sap.ui.core.ValueState.None,
-							ValueStateText: ""
-						}
-					}
-				},
 				headerTitleTable: sHeaderTitleTable,
 				items: []
 			}
